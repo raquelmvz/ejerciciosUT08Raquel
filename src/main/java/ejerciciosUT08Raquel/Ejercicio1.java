dd08b4d94f3017cdfac450890568335de953c9dc
raquelmvz <raquel.mvz98@gmail.com>
2021-04-06 13:47:59 +0200

    Comienzo de proyecto

---
+++ src/main/java/ejerciciosUT08Raquel/Ejercicio1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosUT08Raquel;
+
+/**
+ *
+ * @author raquel
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        /* Fichero a crear - ruta relativa a la carpeta
+        raiz del proyecto */
+        String idFichero = "matrizej1.txt";
+
+        /* Array a escribir */
+        int numeros[][] = {{100, 101, 102, 103},
+        {200, 201, 202, 203},
+        {300, 301, 302, 303},
+        {400, 401, 402, 403}};
+        
+        
+
+    }
+
+}
