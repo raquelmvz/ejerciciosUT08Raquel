539b08abb0b9f8c731d6b8bd65d1826561c14a17
raquelmvz <raquel.mvz98@gmail.com>
2021-04-06 14:02:47 +0200

    Fin ejercicio 1

--- src/main/java/ejerciciosUT08Raquel/Ejercicio1.java
+++ src/main/java/ejerciciosUT08Raquel/Ejercicio1.java
@@ -5,6 +5,10 @@
  */
 package ejerciciosUT08Raquel;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author raquel
@@ -16,14 +20,48 @@ public class Ejercicio1 {
         /* Fichero a crear - ruta relativa a la carpeta
         raiz del proyecto */
         String idFichero = "matrizej1.txt";
+        String tmp;
 
         /* Array a escribir */
         int numeros[][] = {{100, 101, 102, 103},
         {200, 201, 202, 203},
         {300, 301, 302, 303},
         {400, 401, 402, 403}};
-        
-        
+
+        /* Estructura try-with-resources
+        Instancia el objeto con el fichero a escribir
+        y se encarga de cerrar el recurso flujo una vez
+        finalizadas las operaciones */
+        try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+
+            for (int i = 0; i < numeros.length; i++) {
+                for (int j = 0; j < numeros[i].length; j++) {
+
+                    /* Obtengo en un String el elemento de 
+                    la matriz */
+                    tmp = String.valueOf(numeros[i][j]);
+
+                    /* En el ultimo no se pone tabulador */
+                    if (j == numeros[i].length - 1) {
+                        flujo.write(tmp);
+                    } else {
+                        flujo.write(tmp + "\t");
+                    }
+
+                }
+
+                /* new line al acabar cada linea */
+                flujo.newLine();
+
+            }
+
+            /* Metodo flush guarda cambios en el disco */
+            flujo.flush();
+            System.out.println("El fichero se ha creado en la carpeta raiz");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
