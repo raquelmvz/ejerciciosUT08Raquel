3f751ef8483fcee17f1dd00121d15947fa53e1da
raquelmvz <raquel.mvz98@gmail.com>
2021-04-14 13:37:51 +0200

    Modificaciones ej 4

--- src/main/java/ejercicios4y7/Ej4.java
+++ src/main/java/ejercicios4y7/Ej4.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio4;
+package ejercicios4y7;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  *
  * @author raque
  */
-public class Prueba {
+public class Ej4 {
     
     public static void main(String[] args) {
         
@@ -35,25 +35,25 @@ public class Prueba {
         // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
         try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
             
-            flujo.write("Tipo:Vehiculo:Matricula:Marca:Modelo:Color:Tarifa:Disponible");
+            flujo.write("TipoVehiculo:Bastidor:Matricula:Marca:Modelo:Color:Tarifa:Disponible");
             flujo.newLine();
             
             for (Vehiculo veh : vehiculos) {
                 
                 if (veh instanceof Turismo) {
-                    flujo.write("0:" + veh.toString());
+                    flujo.write("0:" + ((Turismo)veh).toString());
                     // Metodo newLine() añade salto de línea después de cada fila
                     flujo.newLine();
                 }
                 
                 if (veh instanceof Deportivo) {
-                    flujo.write("1:" + veh.toString());
+                    flujo.write("1:" + ((Deportivo)veh).toString());
                     // Metodo newLine() añade salto de línea después de cada fila
                     flujo.newLine();
                 }
                 
                 if (veh instanceof Furgoneta) {
-                    flujo.write("2:" + veh.toString());
+                    flujo.write("2:" + ((Furgoneta)veh).toString());
                     // Metodo newLine() añade salto de línea después de cada fila
                     flujo.newLine();
                 }
